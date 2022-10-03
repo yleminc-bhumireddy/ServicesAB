1e683ca9cc9b7652ae9557bbad60e9058225d412
yleminc-bhumireddy <venkata.bhumireddy@yleminc.com>
2022-10-03 18:05:18 -0400

    Removed lambok2.

--- src/main/java/com/mycomp/App.java
+++ src/main/java/com/mycomp/App.java
@@ -1,11 +1,11 @@
 package com.mycomp;
 
-import lombok.extern.slf4j.Slf4j;
+//import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Slf4j
+//@Slf4j
 public class App {
 
     public static void main(String[] args) {
