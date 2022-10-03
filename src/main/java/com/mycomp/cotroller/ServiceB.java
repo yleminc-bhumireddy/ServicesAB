cbe9ff09e2f6e9e64b4d5f513c7f63baa6aab9a5
yleminc-bhumireddy <venkata.bhumireddy@yleminc.com>
2022-10-03 18:07:04 -0400

    Removed lambok2.

--- src/main/java/com/mycomp/cotroller/ServiceB.java
+++ src/main/java/com/mycomp/cotroller/ServiceB.java
@@ -1,8 +1,9 @@
 package com.mycomp.cotroller;
 
-import com.mycomp.com.mycomp.model.PingResponse;
-import com.mycomp.com.mycomp.util.Util;
+
 //import lombok.extern.slf4j.Slf4j;
+import com.mycomp.model.PingResponse;
+import com.mycomp.util.Util;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +18,10 @@ public class ServiceB {
     @GetMapping("/pingB")
     public ResponseEntity<PingResponse> ping() throws IOException {
 //        log.debug("Invoked ping()");
-        PingResponse response = PingResponse.builder()
-                    .hostName(InetAddress.getLocalHost().getHostName())
-                    .ipAddress(Util.getLocalIPAddress()).response(this.getClass().getCanonicalName()).build();
+        PingResponse response = new PingResponse();
+        response.setHostName(InetAddress.getLocalHost().getHostName());
+                    response.setIpAddress(Util.getLocalIPAddress());
+                    response.setResponse(this.getClass().getCanonicalName());
 //        log.debug("Completed ping()");
         return ResponseEntity.ok(response);
     }
