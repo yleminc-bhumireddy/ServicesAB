7db0c36079c875a086211b3748e146a353604c00
yleminc-bhumireddy <venkata.bhumireddy@yleminc.com>
2022-10-15 19:51:27 -0400

    removed sso.

--- src/main/java/com/mycomp/cotroller/ServiceA.java
+++ src/main/java/com/mycomp/cotroller/ServiceA.java
@@ -5,9 +5,9 @@ import com.mycomp.model.PingResponse;
 import com.mycomp.util.Util;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+//import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+//import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -16,7 +16,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import java.io.IOException;
 import java.net.*;
 
-import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
+//import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
 
 @RestController
 @Slf4j
@@ -35,13 +35,14 @@ public class ServiceA {
 
     @GetMapping("/ping")
 //    @PreAuthorize("has")
-    public ResponseEntity<PingResponse> ping(@RegisteredOAuth2AuthorizedClient("myservicesapp_appregistration_app") OAuth2AuthorizedClient authorizedClient) throws IOException, URISyntaxException {
+    public ResponseEntity<PingResponse> ping(/*@RegisteredOAuth2AuthorizedClient("myservicesapp_appregistration_app") OAuth2AuthorizedClient authorizedClient*/) throws IOException, URISyntaxException {
         log.debug("Invoked ping()");
         PingResponse responseBuilder = new PingResponse();
         responseBuilder.setHostName(InetAddress.getLocalHost().getHostName());
         responseBuilder.setIpAddress(Util.getLocalIPAddress());
         responseBuilder.setResponse(this.getClass().getCanonicalName());
-        responseBuilder.setNextResponse(getServiceBResponse(authorizedClient));
+//        responseBuilder.setNextResponse(getServiceBResponse(authorizedClient));
+        responseBuilder.setNextResponse(this.webClient.get().uri("/pingB").retrieve().bodyToMono(PingResponse.class).block());
 //        URI uri = new URI(baseUrl);
 //        ResponseEntity<PingResponse> responseB = restTemplate.getForEntity(uri, PingResponse.class);
 //        if(responseB.getStatusCode().is2xxSuccessful()){
@@ -51,17 +52,17 @@ public class ServiceA {
         return ResponseEntity.ok(responseBuilder);
     }
 
-    private PingResponse getServiceBResponse(OAuth2AuthorizedClient authorizedClient){
-        log.debug("Reading response from ServiceB");
-        PingResponse response = null;
-        try {
-            response = this.webClient.get().uri("https://myservicesab2.azurewebsites.net").attributes(oauth2AuthorizedClient(authorizedClient)).retrieve().bodyToMono(PingResponse.class).block();
-        }catch(Throwable e){
-            log.error(e.getMessage(), e);
-        }
-        log.debug("Completed response from ServiceB");
-        return response;
-    }
+//    private PingResponse getServiceBResponse(OAuth2AuthorizedClient authorizedClient){
+//        log.debug("Reading response from ServiceB");
+//        PingResponse response = null;
+//        try {
+//            response = this.webClient.get().uri("https://myservicesab2.azurewebsites.net").attributes(oauth2AuthorizedClient(authorizedClient)).retrieve().bodyToMono(PingResponse.class).block();
+//        }catch(Throwable e){
+//            log.error(e.getMessage(), e);
+//        }
+//        log.debug("Completed response from ServiceB");
+//        return response;
+//    }
 
     @GetMapping("/")
     public String index(){
